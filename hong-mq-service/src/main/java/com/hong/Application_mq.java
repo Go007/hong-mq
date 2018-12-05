8d795e2d6048a73aec47237742df1a66187f782b
王宏 <wanghong3@dafycredit.com>
2018-12-05 18:44:07 +0800

    yml init

---
+++ hong-mq-service/src/main/java/com/hong/Application_mq.java
@@ -0,0 +1,13 @@
+package com.hong;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application_mq
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
