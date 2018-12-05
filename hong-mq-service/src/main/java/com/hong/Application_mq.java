f236b3e17e0916311dd25d20544c5a2b38263a7a
王宏 <wanghong3@dafycredit.com>
2018-12-05 19:01:03 +0800

    yml init

--- hong-mq-service/src/main/java/com/hong/Application_mq.java
+++ hong-mq-service/src/main/java/com/hong/Application_mq.java
@@ -1,13 +1,13 @@
 package com.hong;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application_mq
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(Application_mq.class, args);
     }
 }
